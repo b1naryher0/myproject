f9a4fac3a282a7b6649f1a99511b8d333b619fef
b1naryher0 <brobinary01@gmail.com>
2019-03-22 18:38:07 +0530

    Character Picture Code Func. ATBS

---
+++ charpictcode.py
@@ -0,0 +1,16 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+rows = len(grid) # no. of elements in the list
+cols = len(grid[0]) # no. of elements in each element of the list
+for j in range(cols):
+    for i in range(rows):
+        print(grid[i][j], end='')
+    print()            
